167cff0c5c5f17eb4934464a4943b16ee1d0504c
Sam Gwydir <gwydir8@gmail.com>
2015-03-11 23:09:04 -0500

    Added some nice variables

--- share/build.sh
+++ share/build.sh
@@ -2,6 +2,29 @@
 # build project3. should be run in root of project.
 # USAGE: cd $PROJECT_ROOT; ./share/build.sh
 
+# get username
+USER=`whoami`
+# get os
+UNAME=`uname`
+# get hostname
+if [ "$UNAME" = "Darwin" ]; then
+    # if os x
+    HOSTNAME=`hostname -s`
+else
+    # if not os x
+    HOSTNAME=`hostname`
+fi
+
+# set number of cores for each server
+if [ "$HOSTNAME" = "sun2.cs.tamu.edu" ]; then
+    CORES=8
+elif [ "$HOSTNAME" = "compute-linux1" ]; then
+    # hell yeah
+    CORES=32
+else
+    CORES=4
+fi
+
 echo "Starting Release Build..."
 
 # create and switch to build dir
@@ -9,8 +32,8 @@ mkdir build;
 cd build;
 # generate release build make files
 cmake -DCMAKE_BUILD_TYPE=Release ..
-# make on four cores
-make -j4
+# make on all cores
+make -j"$CORES"
 echo "Release Build Done!"
 
 echo "Starting Debug Build..."
@@ -22,10 +45,12 @@ mkdir debug;
 cd debug;
 # generate debug build make files
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-make -j4
+# make on all cores
+make -j"$CORES"
+echo "Debug Build Done!"
+
 # cd back to project root
 cd ..
-echo "Debug Build Done!"
 
 echo "Release Build PROJECT_ROOT/build/"
 echo "Debug Build PROJECT_ROOT/debug/"
