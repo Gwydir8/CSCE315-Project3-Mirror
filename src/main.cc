9bddc299dc1a9bbf8fb0a129d6e7ab968ba4a59e
Sam Gwydir <gwydir8@gmail.com>
2015-03-12 19:27:44 -0500

    Fixed main

--- src/main.cc
+++ src/main.cc
@@ -2,36 +2,35 @@
 #include <string>
 
 // gflags
-#define STRIP_FLAG_HELP 1
+// strip help from binary
+//#define STRIP_FLAG_HELP 1
 #include <gflags/gflags.h>
 
-// validates algolrithm flags
-static bool validate_algorithm(const char* flag_name,
-                               const std::string& algol) {
+// validates algorithm flags
+static bool validate_algorithm(const char* flag_name, const std::string& algo) {
   // need to change search to whatever our other choice is
-  if ((algol == "genetic") || (algol == "search")) {
+  if ((algo == "genetic") || (algo == "search")) {
     return true;
   } else {
-    // printf("Invalid value for --%s: %d\n", flagname, (int)value);
+    std::cerr << "Invalid value for --" << std::string(flag_name) << ": "
+              << algo << std::endl;
     return false;
   }
 }
 
 DEFINE_string(algorithm, "genetic", "Choice of algorithm");
+DEFINE_bool(verbose, false, "Print verbose messages");
 
 static const bool algol_validated =
     gflags::RegisterFlagValidator(&FLAGS_algorithm, &validate_algorithm);
 
 int main(int argc, char* argv[]) {
+  // set version
+  gflags::SetVersionString("0.0.1");
   // parse and validate flags
-  gflags::SetUsageMessage(
-      "Group 15: CSCE315-503 Project 3's Circuit Generator.\n"
-      "Usage:\n"
-      "\t--algorithm\tSet the type of algorithm to use (Default: genetic, "
-      "options: genetic, search)\n"
-      "\n"
-      "Example:\n"
-      "\tgauss --algorithm=genetic truth_table.txt\n");
+  std::string usagemsg =
+    std::string( "Usage:\n" ) + std::string(argv[0]) + " [OPTIONS] filename";
+  gflags::SetUsageMessage(usagemsg);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   std::cout << "Group 15: CSCE315-503 Project 3" << std::endl;
