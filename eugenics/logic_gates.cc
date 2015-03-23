3bb7ed069f6982f450e4ee4571c8ec5a946212e5
Jerego Jules Orlino <jjo6212@linux2.cs.tamu.edu>
2015-03-23 16:48:38 -0500

    Logic gates. Will turn ints to bools. Seperate 1 input & 2 input gets into two superclasses.

---
+++ eugenics/logic_gates.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "logic_gates.h"
+using namespace std;
+
+//Evaluation function for the And_Gate
+//It checks all possible combinations
+//Not sure if I should reduce it to just two checks for efficiency.
+int And_Gate::And_Evaluate( int input_1, int input_2 ){
+		if ( input_1 == 0 && input_2 == 0 ){
+			set_output(0);
+		return output;
+	}
+		else if ( input_1 == 0 && input_2 == 1 ){
+			set_output(0);
+		return output;
+	}
+		else if ( input_1 == 1 && input_2 == 0 ){
+			set_output(0);
+		return output;
+	}
+		else if ( input_1 == 1 && input_2 == 1 ){
+			set_output(1);
+		return output;
+	}
+		else
+			cerr << "Invalid Logical Values";
+
+}
+
+//Evaluation function for the Or_Gate
+//It checks all possible combinations
+//Not sure if I should reduce it to just two checks for efficiency.
+int Or_Gate::Or_Evaluate( int input_1, int input_2 ){
+		if ( input_1 == 0 && input_2 == 0 ){
+			set_output(0);
+		return output;
+	}
+		else if ( input_1 == 0 && input_2 == 1 ){
+			set_output(1);
+		return output;
+	}
+		else if ( input_1 == 1 && input_2 == 0 ){
+			set_output(1);
+		return output;
+	}
+		else if ( input_1 == 1 && input_2 == 1 ){
+			set_output(1);
+		return output;
+	}
+		else
+			cerr << "Invalid Logical Values";
+}
+
+//Evaluation function for the Not_Gate
+int Not_Gate::Not_Evaluate( int input_1 ){
+	if input_1 ? return true : return false;
+}
+
+int main() {
+
+}
---
+++ eugenics/logic_gates.h
@@ -0,0 +1,70 @@
+#ifndef LOGIC_GATES_H
+#define LOGIC_GATES_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Logic_Gates{
+	protected:
+		int input_1, input_2;
+		int output;
+	public:
+		//This first constructor used for the NOT Gate. Takes in one input to evaluate.
+		Logic_Gates(const int& input_1) : input_1(input_1);
+
+		//This second constructor used for And and Or Gates. Takes in two inputs to evaluate.
+		Logic_Gates(const int& input_1, const int& input_2) : input_1(input_1), input_2(input_2);
+
+		//Simple Get and Set functions. Don't know if we really need it at the moment.
+		//But it is nice to have them made just in case. 
+		int get_input_1() const { return input_1; }
+		int get_input_2() const { return input_2; }
+		int get_output() const { return output; }
+		void set_input_1( int x ) { input_1 = x; }
+		void set_input_2( int x ) { input_2 = x; }
+		void set_output( int x ) { output = x; }
+};
+
+
+//And Gate: Takes in two inputs. True if both are true. False if this is not the case.
+class And_Gate : public Logic_Gates{
+	protected:
+		int output;
+	public:
+		And_Gate( int x, int y ) : Logic_Gates(input_1, input_2) { }
+		int And_Evaluate( int input_1, int input_2 );
+		void set_output( int x ) { output = x; }
+};
+
+//Or Gate: Takes in two inputs. True if either or both are true. False if this is not the case.
+class Or_Gate : public Logic_Gates{
+	protected:
+		int output;
+	public:
+		Or_Gate( int x, int y) : Logic_Gates(input_1, input_2)  { }
+		int Or_Evaluate( int input_1, int input_2 );
+		void set_output( int x ) { output = x; }
+};
+
+//Not Gate: Takes in one input. Opposite of whatever input is given.
+class Not_Gate : public Logic_Gates{
+	protected:
+		int output;
+	public:
+		Not_Gate( int x ) : Logic_Gates(input_1) { }
+		int Not_Evaluate( int input_1 );
+		void set_input_2() { input_2 = -1; }
+		void set_output( int x ) { output = x; }
+};
+
+#endif
+
+
+
+
+
+
+
