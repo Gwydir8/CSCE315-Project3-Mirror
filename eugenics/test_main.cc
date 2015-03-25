a88e0f1e8b4cdb37af35372be484c73be5f76b5e
Sam Gwydir <sam@samgwydir.com>
2015-03-25 18:50:55 -0500

    stubbed out tests

--- eugenics/test_main.cc
+++ eugenics/test_main.cc
@@ -1,100 +1,16 @@
-//stub
+#include "../../include/gtest/gtest.h"
 
-//#include "../include/gtest/include/gtest/gtest.h"
-#include "../eugenics-system/include/googletest/gtest.h"
+namespace {
+// The fixture for testing class Project3. From google test primer.
+class GateTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
 
-// namespace {
-// // The fixture for testing class Project1. From google test primer.
-// class EngineTest : public ::testing::Test {
-//  protected:
-//   virtual void SetUp() {
-//     Column<std::string> first_col("Scrooge");
-//     first_col.primary_key(true);
-//     first_col.insert_entry("bah");
-//     first_col.insert_entry("hum");
-//     first_col.insert_entry("bug");
-//     first_col.insert_entry("foo");
-//     first_col.insert_entry("bar");
+  // create gates here
+};
 
-//     Column<std::string> second_col("Money");
-//     second_col.insert_entry("$10");
-//     second_col.insert_entry("$100");
-//     second_col.insert_entry("$1000");
-//     second_col.insert_entry("$10000");
-//     second_col.insert_entry("$100000");
-
-//     Column<std::string> third_col("Scrooge");
-//     third_col.primary_key(true);
-//     third_col.insert_entry("1");
-//     third_col.insert_entry("2");
-//     third_col.insert_entry("3");
-//     third_col.insert_entry("4");
-//     third_col.insert_entry("5");
-
-//     Column<std::string> fourth_col("Money");
-//     fourth_col.insert_entry("Money1");
-//     fourth_col.insert_entry("Money2");
-//     fourth_col.insert_entry("Money3");
-//     fourth_col.insert_entry("Money4");
-//     fourth_col.insert_entry("Money5");
-
-//     Column<std::string> fifth_col("Scrooge");
-//     fifth_col.primary_key(true);
-//     fifth_col.insert_entry("1");
-//     fifth_col.insert_entry("4");
-//     fifth_col.insert_entry("5");
-
-//     Column<std::string> sixth_col("Money");
-//     sixth_col.insert_entry("Money1");
-//     sixth_col.insert_entry("Money2");
-//     sixth_col.insert_entry("Money5");
-
-//     Column<std::string> seventh_col("id");
-//     seventh_col.primary_key(true);
-//     seventh_col.insert_entry("1");
-//     seventh_col.insert_entry("2");
-//     seventh_col.insert_entry("3");
-//     seventh_col.insert_entry("4");
-//     seventh_col.insert_entry("5");
-
-//     Column<std::string> eighth_col("Name");
-//     eighth_col.insert_entry("sam");
-//     eighth_col.insert_entry("rabia");
-//     eighth_col.insert_entry("nick");
-//     eighth_col.insert_entry("kade");
-//     eighth_col.insert_entry("leonardo");
-
-//     std::vector<Column<std::string>> columns0;
-//     std::vector<Column<std::string>> columns1;
-//     std::vector<Column<std::string>> columns2;
-//     std::vector<Column<std::string>> columns3;
-//     std::vector<Column<std::string>> columns4;
-
-//     columns0.push_back(first_col);
-//     columns0.push_back(second_col);
-
-//     columns1.push_back(third_col);
-//     columns1.push_back(fourth_col);
-
-//     columns2.push_back(first_col);
-//     columns2.push_back(fourth_col);
-
-//     columns3.push_back(fifth_col);
-//     columns3.push_back(sixth_col);
-
-//     columns4.push_back(seventh_col);
-//     columns4.push_back(eighth_col);
-
-//     db1.createNewTable("Test0", columns0);
-//     db1.createNewTable("Test1", columns1);
-//     db1.createNewTable("Test2", columns2);
-//     db1.createNewTable("Test3", columns3);
-//     db1.createNewTable("Test4", columns4);
-//   }
-
-//   Engine db0;  // Tested for empty
-//   Engine db1;  // Has all the tables
-// };
+  // examples from my dbms
 
 // TEST_F(EngineTest, NoOpenTablesOnInit) { EXPECT_EQ(0, db0.num_open_tables()); }
 
@@ -235,18 +151,9 @@
 //                            db1.select({"Name"}, "Test4")));
 // }
 
-// class ParserTest : public ::testing::Test {
-//  protected:
-//   virtual void SetUp() { Engine db; }
-// };
+}  // Namespace
 
-//   // TEST_F(ParserTests, SQLInsertTest) {
-//   //   EXPECT_
-//   // }
-
-// }  // Namespace
-
-// int main(int argc, char **argv) {
-//   ::testing::InitGoogleTest(&argc, argv);
-//   return RUN_ALL_TESTS();
-// }
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
