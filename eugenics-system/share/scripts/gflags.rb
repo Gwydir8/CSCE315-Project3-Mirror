450d8d73fe6329e69d2081f90d03e30945b3961a
Sam Gwydir <sam@samgwydir.com>
2015-03-27 11:33:26 -0500

    fixed gflags formula

--- eugenics-system/share/scripts/gflags.rb
+++ eugenics-system/share/scripts/gflags.rb
@@ -1,19 +1,22 @@
 require 'formula'
 
-class Gflags < Formula
-  homepage 'http://code.google.com/p/google-gflags/'
-  #url 'https://gflags.googlecode.com/files/gflags-2.0.tar.gz'
-  #sha1 'dfb0add1b59433308749875ac42796c41e824908'
-  url 'https://github.com/schuhschuh/gflags/archive/v2.1.1.tar.gz'
-  sha1 '59b37548b10daeaa87a3093a11d13c2442ac6849'
-  head "https://github.com/schuhschuh/gflags.git"
+class Gflags211 < Formula
+  homepage "http://code.google.com/p/google-gflags/"
+  url "https://github.com/schuhschuh/gflags/archive/v2.1.1.tar.gz"
+  sha1 "59b37548b10daeaa87a3093a11d13c2442ac6849"
+
+  bottle do
+    cellar :any
+    sha1 "a505e4743158a4336b2f4794e724b1f213309aa1" => :yosemite
+    sha1 "c7c39cddb08e025e988c2529e60c5641c9d8eef6" => :mavericks
+    sha1 "d160e728b0c3cf29cb7ed23afe2f20d38deeef57" => :mountain_lion
+  end
 
   depends_on "cmake" => :build
 
   def install
-    mkdir "build" do
-      system "cmake", "..", "-DCMAKE_INSTALL_PREFIX='#{prefix}' -DCMAKE_BUILD_TYPE=None -Wno-dev -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON "
-      system "make", "install"
-    end
+    system "cmake", ".", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON"
+    system "make install"
   end
 end
+
