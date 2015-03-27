60ed182b1cda78d2815ecdd97ecfca63589470a1
Vincent Valenti <vvalenti18@gmail.com>
2015-03-27 00:02:33 -0500

    updated

--- eugenics/bfs.cc
+++ eugenics/bfs.cc
@@ -1,2 +1,17 @@
 
-vector <vector <bools>> rec_function (vector < vector <bools>>)
+#include <vector>
+#include <queue>
+
+queue<circuit> circuits;
+
+vector <vector <bool>> rec_function (vector < vector <bool>> output) {
+
+	// check if output is equal to desired result
+	// PROBLEM: kill node based on criteria
+
+	// PROBLEM: add gates to new circuit 
+	// store the circuit in queue
+
+	// rec_fuction (ciruits.front().get_output())
+
+}
