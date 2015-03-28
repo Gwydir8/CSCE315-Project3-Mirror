f754eee82945418c8e113df569331268c557afa8
Christopher Findeisen <cfindeisen7@gmail.com>
2015-03-28 17:32:58 -0500

    Added mapGateToOutput and tests

--- eugenics/test_algorithm.cc
+++ eugenics/test_algorithm.cc
@@ -1,5 +1,6 @@
 #include "../../local/include/gtest/gtest.h"
 #include "circuit.h"
+#include "gate.h"
 #include "traditional_algorithm.h"
 #include <vector>
 using namespace std;
@@ -7,8 +8,8 @@ TEST(TraditionalAlgorithmTest, ConstructorTest) {
   vector<vector<bool> > expected_output = {{true, false}, {true, false}};
   Circuit basic = Circuit(3,2);
   TraditionalAlgorithm alg = TraditionalAlgorithm(basic, expected_output);
-  /* EXPECT_EQ(expected_output, alg.getOuputSet()); */
-  /* EXPECT_EQ(basic, alg.getRootCircuit()); */
+  EXPECT_EQ(expected_output, alg.getOuputSet());
+  EXPECT_EQ(basic.evaluateAllInputs(), alg.getRootCircuit().evaluateAllInputs());
 }
 TEST(AlgorithmTest, SimpleSearchTest) {
   vector<vector<bool> > expected_output = {
@@ -18,5 +19,5 @@ TEST(AlgorithmTest, SimpleSearchTest) {
   Circuit basic = Circuit(2,2);
   TraditionalAlgorithm alg = TraditionalAlgorithm(basic, expected_output);
   Circuit result = alg.findMatchingCircuit();
-  /* EXPECT_EQ(expected_output, result.evaluateAllInputs()); */
+  EXPECT_EQ(expected_output, result.evaluateAllInputs());
 }
