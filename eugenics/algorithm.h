31fa94b1cc5217b4c3cd74149a4862eb8ff047ca
Jerego Orlino <jjo498@email.tamu.edu>
2015-03-29 13:51:03 -0500

    Created a class for the algorithm. Split it between header and .cc file.

---
+++ eugenics/algorithm.h
@@ -0,0 +1,27 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include <vector>
+#include <queue>
+#include "circuit.h"
+
+class Algo {
+  private:
+    std::queue<Circuit> ex_list;
+    int level = 1;
+    int not_counter = 3;
+    
+  public:
+    Algo();
+
+    int check_output(Circuit x, std::vector<bool> sum, std::vector<bool> c_out);
+    void add_not(int counter);
+    void add_and(int counter);
+    void add_or(int counter);
+    Circuit search(std::vector<bool> sum, std::vector<bool> c_out);
+
+}
+
+
+
+#endif
