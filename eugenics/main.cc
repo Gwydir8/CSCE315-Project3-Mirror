b359b6f017e84cb5ab6d06e0c4643cfdf26b349e
Sam Gwydir <sam@samgwydir.com>
2015-03-28 19:49:27 -0500

    create socket

--- eugenics/main.cc
+++ eugenics/main.cc
@@ -9,17 +9,22 @@
 
 #include <cstdio>
 #include <cstring>
-#include <cstdlib>   //exit
+#include <cstdlib>  //exit
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <netdb.h>
 
 #include "getopts.h"
 #include "utility.h"
 
-int main(int argc, char* argv[]) {
+void error(const char *msg) {
+  perror(msg);
+  std::exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
   // get algorithm from CLI
   Algol_t algol = getOpts(argc, argv);
   if (algol == GENETIC) {
@@ -27,6 +32,44 @@ int main(int argc, char* argv[]) {
   } else if (algol == TRADITIONAL) {
     errlog("Running Traditional Algorithm");
   }
+
+  // create socket
+  int sockfd, portno, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+
+  char buffer[256];
+
+  if (argc < 3) {
+    fprintf(stderr, "usage %s hostname port\n", argv[0]);
+    std::exit(EXIT_SUCCESS);
+  }
+
+  portno = atoi(argv[2]);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) error("ERROR opening socket");
+  server = gethostbyname(argv[1]);
+  if (server == NULL) {
+    fprintf(stderr, "ERROR, no such host\n");
+    std::exit(EXIT_SUCCESS);
+  }
+  bzero((char *)&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,
+        server->h_length);
+  serv_addr.sin_port = htons(portno);
+  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    error("ERROR connecting");
+  printf("Please enter the message: ");
+  bzero(buffer, 256);
+  fgets(buffer, 255, stdin);
+  n = write(sockfd, buffer, strlen(buffer));
+  if (n < 0) error("ERROR writing to socket");
+  bzero(buffer, 256);
+  n = read(sockfd, buffer, 255);
+  if (n < 0) error("ERROR reading from socket");
+  printf("%s\n", buffer);
+  close(sockfd);
 
   std::exit(EXIT_SUCCESS);
 }
