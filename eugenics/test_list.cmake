bbbb34b4f59157d1f7831aa3602f1b322acc4024
Jerego Orlino <jjo498@email.tamu.edu>
2015-03-29 12:00:05 -0500

    update test list

--- eugenics/test_list.cmake
+++ eugenics/test_list.cmake
@@ -1,9 +1,11 @@
 # Unit Tests
 add_executable("test-getopts" src/test_getopts.cc ${SRCS})
 add_executable("test-circuit" src/test_circuit.cc ${SRCS})
+add_executable("test-jv_algorithm" src/test_jv_algorithm.cc ${SRCS})
 # add_executable("test-algorithm" src/test_algorithm.cc ${SRCS})
 
 # linking
 target_link_libraries("test-getopts" gtest gtest_main)
 target_link_libraries("test-circuit" gtest gtest_main)
+target_link_libraries("test-jv_algorithm" gtest gtest_main)
 # target_link_libraries("test-algorithm" gtest gtest_main)
