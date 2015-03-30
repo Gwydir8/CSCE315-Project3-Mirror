e8a364555e3ca7d84aa6d8786d64f9c00b298ee3
Jerego Orlino <jjo498@email.tamu.edu>
2015-03-30 16:13:07 -0500

    commented in debug and build commands at the bottom of the file.

--- eugenics/test_jv_algorithm.cc
+++ eugenics/test_jv_algorithm.cc
@@ -11,16 +11,20 @@ TEST(JVAlgorithmTest, EmptyTest) {
   EXPECT_EQ(expected_output, Algo(Circuit(1,1)).search(expected_output));
 }
 
-// TEST(JVAlgorithmTest, SimpleSearchTest) {
-//   vector<vector<bool>> expected_output = {{false, false},
-//                                           {false, true},
-//                                           {false, true},
-//                                           {true, false},
-//                                           {false, true},
-//                                           {true, false},
-//                                           {true, false},
-//                                           {true, true}};
-//   Algo algorithm_test = Algo(Circuit(3,2));
-//   vector<vector<bool>> result = algorithm_test.search(expected_output);
-//   EXPECT_EQ(expected_output, result);
-// }
+TEST(JVAlgorithmTest, SimpleSearchTest) {
+   vector<vector<bool>> expected_output = {{false, false},
+                                           {false, true},
+                                           {false, true},
+                                           {true, false},
+                                           {false, true},
+                                           {true, false},
+                                           {true, false},
+                                           {true, true}};
+   Algo algorithm_test = Algo(Circuit(3,2));
+   vector<vector<bool>> result = algorithm_test.search(expected_output);
+   EXPECT_EQ(expected_output, result);
+ }
+
+
+///./build/eugenics/test-jv_algorithm
+///./debug/eugenics/test-jv_algorithm
