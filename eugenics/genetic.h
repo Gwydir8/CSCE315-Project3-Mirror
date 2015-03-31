49688a295b5d75e84a0f5c689a20f9a9f6282d8a
Sam Gwydir <sam@samgwydir.com>
2015-03-31 15:36:27 -0500

    add empty genetic algorithm and tests

---
+++ eugenics/genetic.h
@@ -0,0 +1,12 @@
+#ifndef GENETIC_H
+#define GENETIC_H
+
+class Genetic {
+ public:
+  Genetic();
+  virtual ~Genetic();
+
+ private:
+};
+
+#endif /* GENETIC_H */
---
+++ eugenics/test_genetic.cc
@@ -0,0 +1,5 @@
+#include "../../local/include/gtest/gtest.h"
+
+TEST(dumbtest, dumb) {
+  EXPECT_EQ(true, true == true);
+}
