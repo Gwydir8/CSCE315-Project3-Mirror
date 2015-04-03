8827a0deec88feb3ce3e47a81a5e0352fe2e56b9
Sam Gwydir <sam@samgwydir.com>
2015-04-02 19:52:06 -0500

    forgot gate.cc

---
+++ eugenics/gate.cc
@@ -0,0 +1,33 @@
+#include "gate.h"
+
+std::ostream& operator<<(std::ostream& os, const Gate& gate) {
+  // Write circuit to circuitfile
+  if (gate.type != "WIRE") {
+    os << (gate.output_index + 1) << " ";
+    os << gate.type;
+    os << " " << (gate.input_1_index + 1);
+    if (gate.input_2 != nullptr) {
+      os << " " << (gate.input_2_index + 1);
+    }
+    os << std::endl;
+
+    std::string errmsg =
+        "ofstream<<Gate: " + std::to_string((gate.output_index + 1)) + " " +
+        gate.type + " " + std::to_string((gate.input_1_index + 1));
+    // if gate has two inputs, log both
+    if (gate.input_2 != nullptr) {
+      errmsg += " " + std::to_string((gate.input_2_index + 1));
+    }
+    errlog(errmsg);
+  } else {
+    std::string errmsg =
+        "ofstream<<Gate: " + std::to_string((gate.output_index + 1)) + " " +
+        gate.type + " " + std::to_string((gate.input_1_index + 1));
+    if (gate.input_2 != nullptr) {
+      errmsg += " " + std::to_string((gate.input_2_index + 1));
+    }
+    errmsg += " (NOT WRITTEN)";
+    errlog(errmsg);
+  }
+  return os;
+}
