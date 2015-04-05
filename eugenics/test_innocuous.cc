9780f7e5f6c7bcefc1b51ce612278669b0234174
Sam Gwydir <sam@samgwydir.com>
2015-04-04 22:06:46 -0500

    only print every 1000

--- eugenics/test_innocuous.cc
+++ eugenics/test_innocuous.cc
@@ -32,38 +32,17 @@ TEST(InnocuousAttempt, XOR) {
   BooleanTable answer = c->evaluateAllInputs();
   int i = 0;
 
-  // #ifdef THREADED
-  // std::vector<std::future<BooleanTable>> boolean_table_futures;
-  // #endif THREADED
-
   while ((answer != expected_o) || (answer != expected_o_bar)) {
-    // #ifdef THREADED
-    // for (int i = 0; i < (NUM_CORES - 1); ++i) {
-    //   boolean_table_futures.push_back(
-    //       std::async(std::launch::async, , points));
-    // }
-    // #endif THREADED
-
-    // std::vector<Path> paths;  // vector of possible paths
-    // // Calculate a path on main thread while waiting for other threads
-    // // If we're doing more than 1 set, do NUM_SETS random path calculations
-    // on
-    // // main thread
-    // for (int i = 0; i < NUM_SETS; i++) {
-    //   paths.push_back(FindRandomPath(points));
-    // }
-
-    // // Get Paths from threads
-    // for (auto& pathfuture : pathfutures) {
-    //   paths.push_back(pathfuture.get());
-    // }
-
+    ++i;
     delete c;
     c = new GeneticCircuit(2, 1, &rand);
-    std::cerr << "Iteration: " + std::to_string(++i) << std::endl;
-    // std::cerr << *c << std::endl;
+    if (i % 1000 == 0) {
+      std::cerr << "Iteration: " + std::to_string(i) << std::endl;
+      std::cerr << *c << std::endl;
+    }
     answer = c->evaluateAllInputs();
   }
+  EXPECT_EQ(expected_o, answer);
 }
 
 TEST(InnocuousAttempt, FullAdder) {
@@ -89,55 +68,40 @@ TEST(InnocuousAttempt, FullAdder) {
   BooleanTable answer = c->evaluateAllInputs();
   int i = 0;
 
-  // #ifdef THREADED
-  // std::vector<std::future<BooleanTable>> boolean_table_futures;
-  // #endif THREADED
-
   while ((answer != expected_o) || (answer != expected_o_bar)) {
-    // #ifdef THREADED
-    // for (int i = 0; i < (NUM_CORES - 1); ++i) {
-    //   boolean_table_futures.push_back(
-    //       std::async(std::launch::async, , points));
-    // }
-    // #endif THREADED
-
-    // std::vector<Path> paths;  // vector of possible paths
-    // // Calculate a path on main thread while waiting for other threads
-    // // If we're doing more than 1 set, do NUM_SETS random path calculations
-    // on
-    // // main thread
-    // for (int i = 0; i < NUM_SETS; i++) {
-    //   paths.push_back(FindRandomPath(points));
-    // }
-
-    // // Get Paths from threads
-    // for (auto& pathfuture : pathfutures) {
-    //   paths.push_back(pathfuture.get());
-    // }
-
+    ++i;
     delete c;
-    c = new GeneticCircuit(3, 2, &rand);
-    std::cerr << "Iteration: " + std::to_string(++i) << std::endl;
-    // std::cerr << *c << std::endl;
+    c = new GeneticCircuit(2, 2, &rand);
+    if (i % 1000 == 0) {
+      std::cerr << "Iteration: " + std::to_string(i) << std::endl;
+      std::cerr << *c << std::endl;
+    }
     answer = c->evaluateAllInputs();
   }
 }
 
-// TEST(InnocuousAttempt, InvertInputs) {
-//   BooleanTable expected_o = {{true, true, true},
-//                              {true, true, false},
-//                              {true, false, true},
-//                              {true, false, false},
-//                              {false, true, true},
-//                              {false, true, false},
-//                              {false, false, true},
-//                              {false, false, false}};
-//   std::mt19937 rand(std::random_device{}());
-//   // std::minstd_rand0 rand(std::random_device{}());
-//   GeneticCircuit* c = new GeneticCircuit(3, 3, &rand);
-//   while (c->evaluateAllInputs() != expected_o) {
-//     delete c;
-//     c = new GeneticCircuit(3, 3, &rand);
-//     std::cerr << *c << std::endl;
-//   }
-// }
+TEST(InnocuousAttempt, InvertInputs) {
+  BooleanTable expected_o = {{true, true, true},
+                             {true, true, false},
+                             {true, false, true},
+                             {true, false, false},
+                             {false, true, true},
+                             {false, true, false},
+                             {false, false, true},
+                             {false, false, false}};
+  std::mt19937 rand(std::random_device{}());
+  // std::minstd_rand0 rand(std::random_device{}());
+  GeneticCircuit* c = new GeneticCircuit(3, 3, &rand);
+  BooleanTable answer = c->evaluateAllInputs();
+  int i = 0;
+  while ((answer != expected_o)) {
+    ++i;
+    delete c;
+    c = new GeneticCircuit(3, 3, &rand);
+    if (i % 1000 == 0) {
+      std::cerr << "Iteration: " + std::to_string(i) << std::endl;
+      std::cerr << *c << std::endl;
+    }
+    answer = c->evaluateAllInputs();
+  }
+}
