2a687303cc9a79c50dff1b150d61526221a13567
Sam Gwydir <sam@samgwydir.com>
2015-04-04 21:47:16 -0500

    fix clue's hostname

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ elif [ "$HOSTNAME" = "Tron" ]; then
     # CXX="/usr/local/opt/ccache/libexec/clang++-3.6 -std=gnu++11 -stdlib=libc++"
     # #CXXFLAGS="$CXXFLAGS -nostdinc++ -I/usr/local/opt/llvm36/lib/llvm-3.6/include/c++/v1"
     # LDFLAGS="$LDFLAGS -L/usr/local/opt/llvm36/lib/llvm-3.6/lib"
-elif [ "$HOSTNAME" = "Clue" ]; then
+elif [ "$HOSTNAME" = "Clue.local" ]; then
     CORES=12
 else
     CORES=4
