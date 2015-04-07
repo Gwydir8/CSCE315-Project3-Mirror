d93bc403b699526427c6396cd0d635f0b59aaf83
Christopher Findeisen <cfindeisen7@gmail.com>
2015-04-06 22:07:53 -0500

    Full Adder working..

--- eugenics/test_jv_algorithm.cc
+++ eugenics/test_jv_algorithm.cc
@@ -21,30 +21,30 @@ TEST(JVAlgorithmTest, ANDTest) {
 /*   EXPECT_EQ(expected_output, Ckt_Algo(Circuit(2, 1)).search(expected_output)); */
 /* } */
 
-TEST(JVAlgorithmTest, FullAdderTest){
-        vector<vector<bool>> expected_output = {{false, false},
-                                             {false, true},
-                                             {false, true},
-                                             {true, false},
-                                             {false, true},
-                                             {true, false},
-                                             {true, false},
-                                             {true, true}};
-    EXPECT_EQ(expected_output, Ckt_Algo(Circuit(3,2)).search(expected_output));
-}
+/* TEST(JVAlgorithmTest, FullAdderTest){ */
+/*         vector<vector<bool>> expected_output = {{false, false}, */
+/*                                              {false, true}, */
+/*                                              {false, true}, */
+/*                                              {true, false}, */
+/*                                              {false, true}, */
+/*                                              {true, false}, */
+/*                                              {true, false}, */
+/*                                              {true, true}}; */
+/*     EXPECT_EQ(expected_output, Ckt_Algo(Circuit(3,2)).search(expected_output)); */
+/* } */
 
-/*TEST(JVAlgorithmTest, Inverter){
-  vector<vector<bool>> expected_output = {{true, true, true}
-                                          {true, true, false}
-                                          {true, false, true}
-                                          {true, false, false}
-                                          {false, true, true}
-                                          {false, true, false}
-                                          {false, false, true}
+TEST(JVAlgorithmTest, Inverter){
+  vector<vector<bool>> expected_output = {{true, true, true},
+                                          {true, true, false},
+                                          {true, false, true},
+                                          {true, false, false},
+                                          {false, true, true},
+                                          {false, true, false},
+                                          {false, false, true},
                                           {false, false, false}};
 
   EXPECT_EQ(expected_output, Ckt_Algo(Circuit(3, 3)).search(expected_output));
-}*/
+}
 
 
 ///./build/eugenics/test-jv_algorithm
