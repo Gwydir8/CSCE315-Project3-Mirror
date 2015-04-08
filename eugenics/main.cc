f580df3894af79fd4b0a4c6efd019e83383ee849
Sam Gwydir <sam@samgwydir.com>
2015-04-07 19:50:23 -0500

    added genetic algorithm to main

--- eugenics/main.cc
+++ eugenics/main.cc
@@ -8,13 +8,32 @@
 #include <cstdlib>  //exit
 
 #include "getopts.h"
+#include "genetic.h"
+#include "genetic_circuit.h"
+#include "circuit.h"
 #include "utility.h"
 
 int main(int argc, char* argv[]) {
   // get algorithm from CLI
   Algol_t algol = getOpts(argc, argv);
+
   if (algol == GENETIC) {
     errlog("Running Genetic Algorithm");
+
+    std::size_t initial_population_size = 1000;
+    int inputs = 3;
+    BooleanTable expected_outputs = {{false, false},
+                                     {false, true},
+                                     {false, true},
+                                     {true, false},
+                                     {false, true},
+                                     {true, false},
+                                     {true, false},
+                                     {true, true}};
+    Genetic dolly = Genetic(inputs, expected_outputs, initial_population_size);
+
+    std::cout << dolly.evolve() << std::endl;
+
   } else if (algol == TRADITIONAL) {
     errlog("Running Traditional Algorithm");
   }
