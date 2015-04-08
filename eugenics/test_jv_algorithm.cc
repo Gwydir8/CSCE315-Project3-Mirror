d8ae5f240428cb7fc7abe2389d6dac7acbcf25f4
Sam Gwydir <sam@samgwydir.com>
2015-04-07 22:53:11 -0500

    Revert "Temp_mod looking for inverter..."
    This reverts commit edb2e88d0d2f80dfd94ea043cfb6fdb4c0bb4fb9.

--- eugenics/test_jv_algorithm.cc
+++ eugenics/test_jv_algorithm.cc
@@ -5,16 +5,16 @@
 #include <vector>
 using namespace std;
 
-/* TEST(JVAlgorithmTest, EmptyTest) { */
-/*   vector<vector<bool>> expected_output = {{false}, {true}}; */
-/*   EXPECT_EQ(expected_output, Ckt_Algo(Circuit(1, 1)).search(expected_output)); */
-/* } */
+TEST(JVAlgorithmTest, EmptyTest) {
+  vector<vector<bool>> expected_output = {{false}, {true}};
+  EXPECT_EQ(expected_output, Ckt_Algo(Circuit(1, 1)).search(expected_output));
+}
 
 
-/* TEST(JVAlgorithmTest, ANDTest) { */
-/*   vector<vector<bool>> expected_output = {{false}, {false}, {false}, {true}}; */
-/*   EXPECT_EQ(expected_output, Ckt_Algo(Circuit(2, 1)).search(expected_output)); */
-/* } */
+TEST(JVAlgorithmTest, ANDTest) {
+  vector<vector<bool>> expected_output = {{false}, {false}, {false}, {true}};
+  EXPECT_EQ(expected_output, Ckt_Algo(Circuit(2, 1)).search(expected_output));
+}
 
 /* TEST(JVAlgorithmTest, XORTest) { */
 /*   vector<vector<bool>> expected_output = {{false}, {true}, {true}, {false}}; */
